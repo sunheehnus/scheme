0c6298037251859f0f2d3ca09d7fbd85feba40a2
Sun He <sunheehnus@gmail.com>
2014-05-27 06:46:24 +0800

    hano.scm: fix output

--- hano.scm
+++ hano.scm
@@ -2,12 +2,12 @@
   (cond ((= N 0))
 	(else
 	  (MOVE (- N 1) FROM SPARE TO)
-	  (display "MOV ")
-	  (display N)
-	  (display " FROM ")
-	  (display FROM)
-	  (display " TO ")
-	  (display TO)
+	  (display (string-append "MOV "
+				  (number->string N)
+				  " FROM "
+				  FROM
+				  " TO "
+				  TO))
 	  (newline)
 	  (MOVE (- N 1) SPARE TO FROM))))
 (MOVE 4 "start" "target" "tmp")

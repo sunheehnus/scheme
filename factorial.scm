70ed514097f5dd98806d3fba8bec3a55f4ce9531
Sun He <sunheehnus@gmail.com>
2014-06-21 10:58:47 +0800

    format

--- factorial.scm
+++ factorial.scm
@@ -1,14 +1,14 @@
 ;(define (factorial n)
-  ;(if (= n 1)
-	;n
-	;(* n (factorial (- n 1)))))
+;(if (= n 1)
+;n
+;(* n (factorial (- n 1)))))
 
 (define (factorial n)
   (define (iter product counter)
 	(if (> counter n)
 	  product
 	  (iter (* product counter)
-				 (+ counter 1))))
+			(+ counter 1))))
   (iter 1 1))
 
 
--- fib.scm
+++ fib.scm
@@ -1,8 +1,8 @@
 (define (FIB N)
   (if (< N 2)
-      N
-      (+ (FIB (- N 1))
-	 (FIB (- N 2)))))
+	N
+	(+ (FIB (- N 1))
+	   (FIB (- N 2)))))
 (begin
   (display (FIB 10))
   (newline)

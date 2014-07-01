55377b3ac9350308e97d4a27e598fc12ec080e9a
Sun He <sunheehnus@gmail.com>
2014-07-01 17:16:04 +0800

    task 2.4-2.6

---
+++ SICP/2.6.scm
@@ -0,0 +1,9 @@
+(define zero (lambda (f) (lambda (x) x)))
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
+
+(define one (lambda (f) (lambda (x) (f x))))
+(define two (lambda (f) (lambda (x) (f (f x)))))
+
+(define (add para1 para2)
+  (lambda (f) (lambda (x) ((para1 f) ((para2 f) x)))))

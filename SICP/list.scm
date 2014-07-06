27c38c5245f622d90e576da6a2643e6226501128
Sun He <sunheehnus@gmail.com>
2014-07-06 21:25:50 +0800

    task 2.21

--- SICP/list.scm
+++ SICP/list.scm
@@ -74,3 +74,10 @@
 (define (scale-list items factor)
   (map (lambda (x) (* x factor)) items))
 
+(define (square-list items)
+  (define (square x) (* x x))
+  (if (null? items)
+	(list)
+	(cons (square (car items)) (square-list (cdr items)))))
+(define (square-list items)
+  (map (lambda (x) (* x x)) items))
